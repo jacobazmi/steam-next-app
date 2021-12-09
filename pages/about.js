5fa4f62f53e986e1e719519b459089279c18a071
jacobazmi <jacobazmi@gmail.com>
2021-12-09 17:21:52 +0000

    mapping special access

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../stores/authContext";
 
 const About = () => {
-  const { user, authReady } = useContext(AuthContext);
+  const { user, authReady, login } = useContext(AuthContext);
+  const [games, setGames] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (authReady) {
@@ -13,8 +15,20 @@ const About = () => {
           headers: { Authorization: "Bearer " + user.token.access_token },
         }
       )
-        .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("You must be logged in to view this content");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setGames(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setGames(null);
+        });
     }
   }, [user, authReady]);
 
@@ -25,6 +39,21 @@ const About = () => {
       </Head>
       <div>
         <h1>User List</h1>
+        {!authReady && <div>Loading...</div>}
+
+        {error && (
+          <div>
+            <p>{error}</p>
+          </div>
+        )}
+
+        {games &&
+          games.map((game) => (
+            <div key={game.title}>
+              <h3>{game.title}</h3>
+              <h4>Written by {game.platform}</h4>
+            </div>
+          ))}
       </div>
     </>
   );
