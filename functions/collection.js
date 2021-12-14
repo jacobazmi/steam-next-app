45fa13c9ca925e2945b7aa62c56464f232b7e715
jacobazmi <jacobazmi@gmail.com>
2021-12-14 08:56:42 +0000

    first test of databse connection

--- functions/collection.js
+++ functions/collection.js
@@ -1,4 +1,18 @@
+const { useState } = require("react");
+
 exports.handler = async (event, context) => {
+  // const [games, setGames] = useState([]);
+
+  // fetch("localhost:8080/games")
+  // .then((res) => {
+  //   if (res.ok) {
+  //     return res.json();
+  //   }
+  //   throw response;
+  // })
+  // .then((games) => {
+  //   setGames(games);
+  // })
   const games = [
     { title: "Warzone", platform: "PC" },
     { title: "Riders Republic", platform: "PC" },
